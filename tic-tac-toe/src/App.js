c93280eb09eafed6d17cbd78be54184dd6884716
Hemant-cs <hemantchaurasia555@gmail.com>
2025-01-04 22:33:31 +0530

    basic HTML structure

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,10 +1,31 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [currentPlayer, setCurrentPlayer] = useState("X");
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <div className="Title">Tic-Tac-Toe</div>
+      <div className="table">
+        <table>
+          <tr>
+            <td index="0">h1</td>
+            <td index="1">h2</td>
+            <td index="2">h3</td>
+          </tr>
+          <tr>
+            <td index="3">h4</td>
+            <td index="4">h5</td>
+            <td index="5">h6</td>
+          </tr>
+          <tr>
+            <td index="6">h7</td>
+            <td index="7">h8</td>
+            <td index="8">h9</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
